29628994ea5bde815f0c9a563ad0f02219eebe51
tauseefk <tauseef25@gmail.com>
2022-08-21 23:19:37 -0700

    export derive macro

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,16 @@ mod prelude {
 }
 
 pub use prelude::*;
+
+// Re-export #[derive(AnimationTransitionMacro)].
+//
+// The reason re-exporting is not enabled by default is that disabling it would
+// be annoying for crates that provide handwritten impls or data formats. They
+// would need to disable default features and then explicitly re-enable std.
+#[cfg(feature = "animation_transition_derive")]
+#[allow(unused_imports)]
+#[macro_use]
+extern crate animation_transition_derive;
+#[cfg(feature = "animation_transition_derive")]
+#[doc(hidden)]
+pub use animation_transition_derive::*;
